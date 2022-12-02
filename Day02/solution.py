9195aacdf589b728d55f27d0a8164e360ad70045
Peter Romančík <romancikpeter@gmail.com>
2022-12-02 10:40:30 +0100

    Change indexing of selected shapes

--- Day02/solution.py
+++ Day02/solution.py
@@ -1,5 +1,5 @@
-elf_values = { "A": 1, "B": 2, "C": 3}
-me_values = { "X": 1, "Y": 2, "Z": 3} 
+elf_values = { "A": 0, "B": 1, "C": 2}
+part1_values = { "X": 0, "Y": 1, "Z": 2} 
 part2_values = { "X": -1, "Y": 0, "Z": 1 }
 
 def calculate_score(me: int, elf: int) -> int:
@@ -10,27 +10,21 @@ def calculate_score(me: int, elf: int) -> int:
     return 0
 
 def main() -> None:
-    score = 0
+    score1 = 0
     score2 = 0
 
     with open("input.txt", "r") as file:
         for line in file:
             line = line.strip();
             elf = elf_values[line[0]]
-            me = me_values[line[2]]
-
-            score += me + calculate_score(me, elf)
+            me = part1_values[line[2]]
+            score1 += me + 1 + calculate_score(me, elf)
 
             # part 2 
-            me2 = elf + part2_values[line[2]]
-            if me2 > 3:
-                me2 = 1
-            elif me2 < 1:
-                me2 = 3
+            me2 = (elf + part2_values[line[2]]) % 3
+            score2 += me2 + 1 + calculate_score(me2, elf)
 
-            score2 += me2 + calculate_score(me2, elf)
-
-    print(score)
+    print(score1)
     print(score2)
 
 if __name__ == "__main__":
